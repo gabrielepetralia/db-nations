a55e981114d4f7b8b3b360536319c366172bbea9
gabrielepetralia <gabri.petralia03@gmail.com>
2023-09-28 17:01:34 +0200

    milestone 4

--- src/main/java/org/java/nations/Main.java
+++ src/main/java/org/java/nations/Main.java
@@ -5,6 +5,8 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -18,12 +20,14 @@ public class Main {
 			
 			Scanner sc = new Scanner(System.in);
 			
-			System.out.print("Inserisci una stringa per filtrare le nazioni: ");
+			System.out.print("Enter a string to filter the countries: ");
 			String strFilter = sc.nextLine();
+			
+			String sqlQuery = null;
 			
 			System.out.println("\n---------------------------------------------\n");
 			
-			final String sqlQuery = " SELECT c.country_id, c.name AS 'country', r.name AS 'region', c2.name AS 'continent' "
+			sqlQuery = " SELECT c.country_id, c.name AS 'country', r.name AS 'region', c2.name AS 'continent' "
 					 		 	+ " FROM countries c  "
 								+ " JOIN regions r ON c.region_id = r.region_id "
 								+ "	JOIN continents c2 ON r.continent_id = c2.continent_id "
@@ -47,6 +51,82 @@ public class Main {
 					System.out.println("Country: " + country);
 					System.out.println("Region: " + region);
 					System.out.println("Continent: " + continent);
+					
+					System.out.println("\n---------------------------------------------\n");
+				} 
+				
+			}catch (Exception e) {
+				System.out.println(e.getMessage());
+			}
+			
+			System.out.print("Enter a country ID to show details: ");
+			int selectedId = Integer.valueOf(sc.nextLine());
+			
+			System.out.println("\n---------------------------------------------\n");
+			
+			sqlQuery = " SELECT l.`language` "
+					 	+ " FROM countries c "
+					 	+ " JOIN country_languages cl "
+					 	+ " ON c.country_id = cl.country_id "
+					 	+ " JOIN languages l "
+					 	+ " ON cl.language_id = l.language_id "
+					 	+ " WHERE c.country_id = ? ";
+			
+			List<String> languages = new ArrayList<>();
+			
+			try {
+				PreparedStatement ps = conn.prepareStatement(sqlQuery);	
+				ps.setInt(1, selectedId);
+				
+				ResultSet rs = ps.executeQuery();
+				
+				while(rs.next()) {
+					languages.add(rs.getString("language"));
+				}
+			}catch (Exception e) {
+				System.out.println(e.getMessage());
+			}
+			
+			sqlQuery = " SELECT c.name, cs.`year`, cs.population, cs.gdp "
+						+ " FROM countries c "
+						+ " JOIN country_stats cs  ON  c.country_id  = cs.country_id "
+						+ " WHERE c.country_id = ? "
+						+ "	AND cs.`year` = ( "
+						+ "		SELECT cs.`year` "
+						+ "		FROM countries c "
+						+ "		JOIN country_stats cs  ON  c.country_id  = cs.country_id "
+						+ "		WHERE c.country_id = ? "
+						+ "		ORDER BY cs.`year` DESC "
+						+ "		LIMIT 1	)";
+			
+			try {
+				PreparedStatement ps = conn.prepareStatement(sqlQuery);	
+				ps.setInt(1, selectedId);
+				ps.setInt(2, selectedId);
+				
+				ResultSet rs = ps.executeQuery();
+				
+				while(rs.next()) {
+					
+					String country = rs.getString("name");
+					int year = rs.getInt("year");
+					int population = rs.getInt("population");
+					long gdp = rs.getLong("gdp");
+					
+					System.out.println("Details for: " + country + "\n");
+					
+					System.out.print("Languages: ");
+					
+					for (int i=0; i<languages.size(); i++) {
+						System.out.print(languages.get(i));
+						if(i<languages.size() - 1) {
+							System.out.print(", ");
+						}
+					}
+					
+					System.out.println("\nYear: " + year);
+					System.out.println("Population: " + population);
+					System.out.println("GDP: " + gdp);
 					
 					System.out.println("\n---------------------------------------------\n");
 				} 
