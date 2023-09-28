fb71987f24a66adbad6d5b013dd65d7708e52199
gabrielepetralia <gabri.petralia03@gmail.com>
2023-09-28 15:49:30 +0200

    milestone 3

--- src/main/java/org/java/nations/Main.java
+++ src/main/java/org/java/nations/Main.java
@@ -5,6 +5,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) throws SQLException {
@@ -15,14 +16,24 @@ public class Main {
 		
 		try (Connection conn = DriverManager.getConnection(url, user, password)) {
 			
+			Scanner sc = new Scanner(System.in);
+			
+			System.out.print("Inserisci una stringa per filtrare le nazioni: ");
+			String strFilter = sc.nextLine();
+			
+			System.out.println("\n---------------------------------------------\n");
+			
 			final String sqlQuery = " SELECT c.country_id, c.name AS 'country', r.name AS 'region', c2.name AS 'continent' "
 					 		 	+ " FROM countries c  "
 								+ " JOIN regions r ON c.region_id = r.region_id "
 								+ "	JOIN continents c2 ON r.continent_id = c2.continent_id "
+								+ " WHERE c.name LIKE ? "
 								+ "	ORDER BY c.name " ;
 			
 			try {
-				PreparedStatement ps = conn.prepareStatement(sqlQuery);			
+				PreparedStatement ps = conn.prepareStatement(sqlQuery);	
+				ps.setString(1, "%" + strFilter + "%");
+				
 				ResultSet rs = ps.executeQuery();
 				
 				while(rs.next()) {
@@ -32,7 +43,7 @@ public class Main {
 					String region = rs.getString("region");
 					String continent = rs.getString("continent");
 					
-					System.out.println("Coutry ID: " + countryId + "\n");
+					System.out.println("Country ID: " + countryId + "\n");
 					System.out.println("Country: " + country);
 					System.out.println("Region: " + region);
 					System.out.println("Continent: " + continent);
@@ -43,8 +54,10 @@ public class Main {
 			}catch (Exception e) {
 				System.out.println(e.getMessage());
 			}
-		}
 			
+		}
+		
+				
 	}		
 
 }
